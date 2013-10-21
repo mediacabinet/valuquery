1d984e5b52cdadcf787e4de83987073f54c84eaf
Juha Suni <juha.suni@mediacabinet.fi>
2013-10-21 16:52:10 +0300

    Several fixes

--- src/ValuQuery/DoctrineMongoOdm/QueryListener.php
+++ src/ValuQuery/DoctrineMongoOdm/QueryListener.php
@@ -3,54 +3,80 @@ namespace ValuQuery\DoctrineMongoOdm;
 
 use ValuQuery\QueryBuilder\Event\SimpleSelectorEvent;
 use ValuQuery\QueryBuilder\Event\SequenceEvent;
+use ValuQuery\QueryBuilder\Event\QueryBuilderEvent;
 use ValuQuery\MongoDb\QueryListener as BaseListener;
 use Doctrine\ODM\MongoDB\Types\Type;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use ArrayAccess;
 
 class QueryListener extends BaseListener
 {
+    const QUERY_PARAM_NS = '__doctrine_mongodb_odm';
+    
+    /**
+     * @var DocumentManager
+     */
+    protected $documentManager;
+    
+    /**
+     * Discriminator map
+     * 
+     * @var array
+     */
+    protected $discriminatorMap;
+    
+    public function __construct(DocumentManager $dm)
+    {
+        $this->setDocumentManager($dm);
+    }
+    
+    public function attach(EventManagerInterface $events)
+    {
+        parent::attach($events);
+        
+        $this->listeners[] = $events->attach(
+            'finalizeQuery',
+            array(
+                $this,
+                'finalizeQuery'
+            ));
+    }
+    
+    public function finalizeQuery(QueryBuilderEvent $event)
+    {
+        // Remove any custom parameters from the query
+        $query = $event->getQuery();
+        unset($query[self::QUERY_PARAM_NS]);
+    }
+    
     public function applyElementSelector(SimpleSelectorEvent $event)
     {
         $elementSelector = $event->getSimpleSelector();
-        $document = $this->getDocumentNameForElement($elementSelector->getValue());
-    
-        $event->setQuery(
-                $this->createQueryBuilder($document)->expr()
-        );
-    }
-    
-    public function applyIdSelector(SimpleSelectorEvent $event)
-    {
-        $idSelector = $event->getSimpleSelector();
-        $element = $this->requireElement();
-        $class = $this->getElementMetadata($element);
-        $field = $class->getIdentifier();
-    
-        if ($field) {
-    
-            if ($class->isIdGeneratorAuto()) {
-                $condition = new \MongoId($idSelector->getCondition());
-            } else {
-                // TODO: might it be that there are user-generated MongoIds as
-                // well?
-                $condition = $idSelector->getCondition();
-            }
-    
-            $selector = new SimpleSelector\Attribute(
-                    $field,
-                    SimpleSelector\Attribute::OPERATOR_EQUALS,
-                    $condition);
-    
-            $this->doApplyAttributeSelector($selector, $event->getQuery());
-    
+        $class = $this->getElementMetadata($elementSelector->getValue());
+        
+        if ($class) {
+            $discrField = $class->discriminatorField['name'];
+            $discrValue = $class->discriminatorValue;
+            
+            $query = $event->getQuery();
+            $this->applyQueryCommand($query, $discrField, null, $discrValue);
+            
+            $this->setQueryParam(
+                $query,
+                'documentName',
+                $this->getDocumentNameForElement($elementSelector->getValue())
+            );
+            
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
     
     public function applyPathSelector(SimpleSelectorEvent $event)
     {
         $pathSelector = $event->getSimpleSelector();
+        $query = $event->getQuery();
         
         if (sizeof($pathSelector->getPathItems())) {
             $path = $this->translatePathArray($pathSelector->getPathItems());
@@ -65,23 +91,24 @@ class QueryListener extends BaseListener
                 return $this->doApplyAttributeSelector($selector, $event->getQuery());
             }
             
-            $condition = '^' . SimpleSelector\Path::PATH_SEPARATOR .
+            $pattern = '^' . SimpleSelector\Path::PATH_SEPARATOR .
                          implode(SimpleSelector\Path::PATH_SEPARATOR, $path) . 
                          '$';
-            
-            $selector = new SimpleSelector\Attribute(
-                $this->getOption('path_attribute'), 
-                SimpleSelector\Attribute::OPERATOR_REG_EXP, 
-                $condition);
+          
+            $this->applyQueryCommand(
+                $query, 
+                $this->getPathField(), 
+                'regex', 
+                $pattern);
         } else {
-            // Query root
-            $selector = new SimpleSelector\Attribute(
-                $this->getOption('path_attribute'), 
-                SimpleSelector\Attribute::OPERATOR_EQUALS, 
+            $this->applyQueryCommand(
+                $query, 
+                $this->getPathField(), 
+                null, 
                 SimpleSelector\Path::PATH_SEPARATOR);
         }
         
-        return $this->doApplyAttributeSelector($selector, $event->getQuery());
+        return true;
     }
     
     /**
@@ -89,12 +116,76 @@ class QueryListener extends BaseListener
      *
      * @return DocumentManager
      */
-    protected function getDocumentManager()
+    public function getDocumentManager()
     {
         return $this->documentManager;
     }
+    
+    public function setDocumentManager(DocumentManager $dm)
+    {
+        $this->documentManager = $dm;
+    }
 
-    protected function createQueryBuilder($documentName = null)
+    /**
+     * @return multitype:
+     */
+    public function getDiscriminatorMap()
+    {
+        return $this->discriminatorMap;
+    }
+
+	/**
+     * @param multitype: $discriminatorMap
+     */
+    public function setDiscriminatorMap($discriminatorMap)
+    {
+        $this->discriminatorMap = $discriminatorMap;
+    }
+    
+    /**
+     * @see \ValuQuery\MongoDb\QueryListener::filterField()
+     */
+    protected function filterField(ArrayAccess $query, $field, $value)
+    {
+        $documentName = $this->getQueryParam(
+            $query, 
+            'documentName', 
+            $this->getDefaultDocumentName());
+        
+        if ($documentName) {
+            
+            $meta   = $this->getDocumentManager()->getClassMetadata($documentName);
+            $fields = explode('.', $field);
+            
+            foreach ($fields as $index => $fieldName) {
+                if ($meta->hasAssociation($fieldName)) {
+                    $fieldMapping = $meta->getFieldMapping($fieldName);
+                    $meta =  $this->getDocumentManager()->getClassMetadata($fieldMapping['targetDocument']);
+                } elseif($index === (sizeof($fields)-1)) {
+                    $type = $meta->getTypeOfField($fieldName);
+                    
+                    if (!$type) {
+                        foreach ($meta->parentClasses as $class)
+                        {
+                            $type = $this->getDocumentManager()->getClassMetadata($class)->getTypeOfField($fieldName);
+                            if($type) break;
+                        }
+                    }
+                    
+                    if ($type && $type !== 'collection' && $type !== 'one') {
+                        $value = Type::getType($type)->convertToDatabaseValue($value);
+                    }
+                } else {
+                    throw new Exception\UnknownFieldException(
+                        sprintf("Unknown field '%s'", $field));
+                }
+            }
+        }
+        
+        return $value;
+    }
+    
+	protected function createQueryBuilder($documentName = null)
     {
         return $this->getDocumentManager()->createQueryBuilder($documentName);
     }
@@ -108,9 +199,15 @@ class QueryListener extends BaseListener
      */
     protected function getDocumentNames()
     {
-        return $this->getSequence()->getDocumentNames();
+        return array_values($this->getDiscriminatorMap());
     }
     
+    /**
+     * Retrieve document name for element name
+     * 
+     * @param string $element
+     * @return string|null
+     */
     protected function getDocumentNameForElement($element)
     {
         $names = $this->getDocumentNames();
@@ -135,28 +232,6 @@ class QueryListener extends BaseListener
     }
 
     /**
-     * Map attribute name to corresponding
-     * field name in MongoDB
-     *
-     * @param string $attr            
-     * @return string
-     */
-    protected function mapAttribute($attr)
-    {
-        $map = $this->getOption('attribute_map');
-    
-        /**
-         * Map attribute name
-        */
-    
-        if(array_key_exists($attr, $map)){
-            $attr = $map[$attr];
-        }
-    
-        return $attr;
-    }
-    
-    /**
      * Template method for path translation
      *
      * @param array $items
@@ -176,18 +251,17 @@ class QueryListener extends BaseListener
             // Create a new Query Builder instance
             $qb = $this->getDocumentManager()->createQueryBuilder($documentName);
             
-            $attr = $this->mapAttribute($this->getOption('path_attribute'));
+            $field = $this->getPathField();
             
             // Select path attribute
-            $qb->select($attr)->hydrate(false);
+            $qb->select($field)->hydrate(false);
             
             // Use template to create a new selector query
             $template = $this->getSequence()->getSelectorTemplate();
             $selector = $template->createSelector($items[0]);
-            $selector->setDocumentNames(
-                    array(
-                            Sequence::DEFAULT_ELEMENT => $documentName
-                    ));
+            $selector->setDocumentNames([
+                Sequence::DEFAULT_ELEMENT => $documentName
+            ]);
             $selector->extendQuery($qb);
             
             // Fetch data
@@ -196,11 +270,11 @@ class QueryListener extends BaseListener
                 ->getSingleResult();
             
             // Exit early if selector could not be resolved
-            if (! $result || ! isset($result[$attr]) || ! $result[$attr]) {
+            if (! $result || ! isset($result[$field]) || ! $result[$field]) {
                 return false;
             }
             
-            $path = ltrim($result[$attr], '/');
+            $path = ltrim($result[$field], '/');
             
             unset($items[0]);
             $items = array_map('stripslashes', $items);
@@ -222,5 +296,35 @@ class QueryListener extends BaseListener
         }
         
         return $items;
+    }
+
+    /**
+     * Set custom query parameter
+     * 
+     * @param ArrayAccess $query
+     * @param string $param
+     * @param mixed $value
+     */
+    private function setQueryParam($query, $param, $value)
+    {
+        $query[self::QUERY_PARAM_NS][$param] = $value;
+    }
+    
+    /**
+     * Retrieve custom query parameter
+     * +
+     * @param ArrayAccess $query
+     * @param string $param
+     * @param mixed $default
+     * @return string
+     */
+    private function getQueryParam(ArrayAccess $query, $param, $default = null)
+    {
+        if (isset($query[self::QUERY_PARAM_NS]) 
+            && array_key_exists($param, $query[self::QUERY_PARAM_NS])) {
+            return $query[self::QUERY_PARAM_NS][$param];
+        } else {
+            return $default;
+        }
     }
 }
