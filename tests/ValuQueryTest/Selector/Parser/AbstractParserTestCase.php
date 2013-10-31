b78ec981c2cfb859bf136dbaef77772d01692f52
Juha Suni <juha.suni@mediacabinet.fi>
2013-10-31 15:30:41 +0200

    Removes unused dependency

--- tests/ValuQueryTest/Selector/Parser/AbstractParserTestCase.php
+++ tests/ValuQueryTest/Selector/Parser/AbstractParserTestCase.php
@@ -1,8 +1,6 @@
 <?php
 namespace ValuQueryTest\Selector\Parser;
 
-use ValuQuery\Selector\Parser\AttributeSelectorParser;
-
 abstract class AbstractParserTestCase 
     extends \PHPUnit_Framework_TestCase{
 
