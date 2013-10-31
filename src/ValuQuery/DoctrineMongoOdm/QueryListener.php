0663d603363cdadeeded3c9136242ad8d810a600
Juha Suni <juha.suni@mediacabinet.fi>
2013-10-31 15:14:57 +0200

    Removes unused dependency

--- src/ValuQuery/DoctrineMongoOdm/QueryListener.php
+++ src/ValuQuery/DoctrineMongoOdm/QueryListener.php
@@ -5,7 +5,6 @@ use ValuQuery\QueryBuilder\Event\SimpleSelectorEvent;
 use ValuQuery\QueryBuilder\Event\QueryBuilderEvent;
 use ValuQuery\MongoDb\QueryListener as BaseListener;
 use ValuQuery\Selector\SimpleSelector;
-use ValuQuery\Selector\Selector;
 use Doctrine\ODM\MongoDB\Types\Type;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
