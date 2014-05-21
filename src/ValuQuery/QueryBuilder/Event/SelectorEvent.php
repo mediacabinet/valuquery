2a59380810524d7450ccb623e3cfaa2511338d5c
Juha Suni <juha.suni@mediacabinet.fi>
2014-05-21 15:07:36 +0300

    Adds convenience methods for getting/setting sequence

--- src/ValuQuery/QueryBuilder/Event/SelectorEvent.php
+++ src/ValuQuery/QueryBuilder/Event/SelectorEvent.php
@@ -1,5 +1,21 @@
 <?php
 namespace ValuQuery\QueryBuilder\Event;
 
+use ValuQuery\Selector\Sequence;
+
 class SelectorEvent extends QueryBuilderEvent
-{}
+{
+    public function setSequence(Sequence $sequence)
+    {
+        return $this->setParam('sequence', $sequence);
+    }
+    
+    /**
+     * @return Sequence
+     */
+    public function getSequence()
+    {
+        return $this->getParam('sequence');
+    }
+    
+}
